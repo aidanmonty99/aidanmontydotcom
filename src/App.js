e7fa6ae875e1b4207177b715ab58c587a220479e
Aidan Monty <montyaidan@gmail.com>
2024-09-15 16:49:24 -0500

    Build intro

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import profile_pic from './img/Me in the Scottish highlands.jpg';
+import my_picture from './img/Me in the Scottish highlands.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={profile_pic} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my website! Come back soon to see more!
-        </p>
-      </header>
+      <div className="App-header">
+        <header className="App-header-text">
+          Hi! My name is Aidan Monty, and I'm a developer with an eye for detail.
+        </header>
+        <img src={my_picture} className="My-picture" alt="My picture" />
+      </div>
     </div>
   );
 }
