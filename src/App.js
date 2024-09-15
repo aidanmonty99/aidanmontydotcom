d5c6a711b0d0642eb913a1bd4d1d904663a9d66d
Aidan Monty <montyaidan@gmail.com>
2024-09-15 17:24:41 -0500

    Add ease in animation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <header className="App-header-text">
+      <div className="App-header ease-in">
+        <header className="App-header-text ease-in">
           Hi! My name is Aidan Monty, and I'm a developer with an eye for detail.
         </header>
-        <img src={my_picture} className="My-picture" alt="My picture" />
+        <img src={my_picture} className="My-picture ease-in" alt="My picture" />
       </div>
       {/* <hr /> */}
+
 
     </div>
   );
